bdee47794d530b6fec0d20608cb365c009e3f1b7
ztevivo <104692280+ztevivo@users.noreply.github.com>
2026-06-22 23:14:32 -0300

    Atualizar o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
